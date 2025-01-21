27afeb06d0413998226a77b1c754befdf3dbaf0b
Nest <58371847+Nest4u@users.noreply.github.com>
2025-01-21 17:49:14 +0100

    Update apiService.tsx

--- src/services/apiService.tsx
+++ src/services/apiService.tsx
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
- const API_URL = 'https://back-trood.onrender.com';
+
 
 export const api = axios.create({
-	baseURL: API_URL || '/api',
+	baseURL: '/api',
 	headers: {
 		'Content-Type': 'application/json'
 	}
